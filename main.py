39a48505d6bba815167a453ba402aa1109fb2410
aadityakalwani <aadityakalwani@gmail.com>
2021-08-01 15:15:40 +0100

    added ways to open youtube and then an input to open a new google tab

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 import webbrowser
 
-webbrowser.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
 
-nice
+webbrowser.open("https://www.youtube.com")
+
+
+def new_tab():
+    webbrowser.open("https://www.google.com")
+
+
+on = str(input("Press x to open a new tab: "))
+
+if on.lower() == "x":
+    new_tab()
